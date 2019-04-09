4a71a82dc291bef95c9919598b272e9526b0c145
bobnearents <bobnearents@yahoo.com>
2019-04-08 22:01:52 -0700

    refactored to use flexbox

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-$(
+/* eslint-disable no-undef */
+'use strict';
 
-  $('.personal-description').on('mouseover', function() {
 const description = ['a full-stack web developer', 'Bob Nearents', 'a cat enthusiast', 'a husband', 'a dad'];
-    
-    $(this).html(description[Math.floor(Math.random() * 5)]);
-})
-
-)
+//randomly fill a queue with 'description' and display that way instead.
+//It will still be random but will avoid duplicates in a cycle
+setInterval(function () {
+  let i = Math.round((Math.random()) * description.length);
+  if (i === description.length)--i;
+  $('.personal-description').html(description[i]);
+}, 3000);
